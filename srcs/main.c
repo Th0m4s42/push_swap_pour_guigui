c25efd9d079a280cfc4446ed489657b7bfa96e30
Thomas <thbasse@student.42.fr>
2024-07-22 16:54:44 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:41:07 by thbasse           #+#    #+#             */
-/*   Updated: 2024/07/22 16:40:15 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/07/22 16:54:07 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ int	main(int argc, char **argv)
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
+	if (argc == 1)
 		return (1);
 	else if (argc == 2)
+	{
 		argv = ft_split(argv[1], ' ');
+		if (argv == NULL)
+			return (1);
+	}
 	else
 		argv++;
 	init_stack_a(&a, argv, argc);
