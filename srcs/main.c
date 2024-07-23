0abf037052610e66861f6d05bd9bfa3fc712d642
Thomas <thbasse@student.42.fr>
2024-07-23 14:14:19 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,21 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:41:07 by thbasse           #+#    #+#             */
-/*   Updated: 2024/07/22 16:54:07 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/07/23 14:07:10 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+// static void perform_sort(t_stack_node *a, t_stack_node *b)
+// {
+// 	if (stack_len(a) == 2)
+// 		sa(&a);
+// 	else if (stack_len(a) == 3)
+// 		sort_three(&a);
+// 	else
+// 		sort_stacks(&a, &b);
+// }
 
 int	main(int argc, char **argv)
 {
@@ -24,8 +34,8 @@ int	main(int argc, char **argv)
 	else if (argc == 2)
 	{
 		argv = ft_split(argv[1], ' ');
-		if (argv == NULL)
-			return (1);
+			if (argv == NULL)
+				return (1);
 	}
 	else
 		argv++;
@@ -40,10 +50,7 @@ int	main(int argc, char **argv)
 			sort_stacks(&a, &b);
 	}
 	if (argc == 2)
-	{
-		free(argv[0]);
-		free(argv);
-	}
+		free_argv(argv);
 	free_stack(&a);
 	return (0);
 }
