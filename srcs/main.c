a2f1d4f307f1101bb1ce30340dfa0c4430c3ad5d
Thomas <thbasse@student.42.fr>
2024-07-23 16:17:44 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,21 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:41:07 by thbasse           #+#    #+#             */
-/*   Updated: 2024/07/23 14:25:27 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/07/23 16:15:30 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-// static void perform_sort(t_stack_node *a, t_stack_node *b)
-// {
-// 	if (stack_len(a) == 2)
-// 		sa(&a);
-// 	else if (stack_len(a) == 3)
-// 		sort_three(&a);
-// 	else
-// 		sort_stacks(&a, &b);
-// }
+static void	inter(t_stack_node **a, t_stack_node **b)
+{
+	if (stack_len(*a) == 2)
+		sa(a);
+	else if (stack_len(*a) == 3)
+		sort_three(a);
+	else
+		sort_stacks(a, b);
+}
 
 int	main(int argc, char **argv)
 {
@@ -41,14 +41,7 @@ int	main(int argc, char **argv)
 		argv++;
 	init_stack_a(&a, argv, argc);
 	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a);
-		else if (stack_len(a) == 3)
-			sort_three(&a);
-		else
-			sort_stacks(&a, &b);
-	}
+		inter(&a, &b);
 	if (argc == 2)
 		free_argv(argv);
 	free_stack(&a);
